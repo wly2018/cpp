5d57e64efb4f295439dd35bcaf91ca4e07929d58
wly2018 <1419947026@qq.com>
2019-12-15 22:11:16 +0800

    change

--- test.cpp
+++ test.cpp
@@ -3,27 +3,13 @@
 #include<math.h>
 using namespace std;
 
-int tran(char s)
-{
-	if (s>='0' && s<='9') return s-'0';
-	if (s>='a' && s<='z')
-	return s-'a'+10;
-	else return -1;
-}
-
-long power(int x, int k){
-	int s=1;
-	for (int i=0;i<k;i++)
-	s=s*x;
-	return s;
-}
 
 int main()
 {
 	string s1,s2,s3;
 	int tag,ra,i,j,len,maxi;
 	bool flag;
-	long sum1,sum2;
+	long k1,k2;
 	
 	s1="121";
 	s2="34";
